ed2bc29f0a5973750ed10f7673d91a1a9b505678
unknown <zkryabas174@gmail.com>
2023-01-28 03:48:24 +0200

    Tow commit

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use function Pest\Laravel\json;
 
 class PostController extends Controller
@@ -31,6 +32,59 @@ class PostController extends Controller
 
 
 
+
+
+  }
+
+
+  public function store(Request $request){
+
+
+      $validate=  validator::make($request->all(),[
+          'title'=>['required'],
+          'body'=>['required'],
+      ]);
+
+     if ($validate->fails()){
+         return $this->ShowResponse(null,$validate->errors(),400);
+
+
+     }
+
+       $post=Post::create($request->all());
+
+       if ($post){
+
+           return $this->ShowResponse(new PostResource($post),"The post Save",201);
+       }
+
+      return $this->ShowResponse(null,"The post Not Save",400);
+
+  }
+
+
+  public function destroy($id){
+
+       $post=Post::where('id',$id)->delete();
+
+       if ($post){
+
+           return $this->ShowResponse(null,"تمت العملية بنجاح",250);
+       }
+
+      return $this->ShowResponse(null,"فشلت العملية ",240);
+
+  }
+
+  public function update(Request $request,$id){
+
+       $post=Post::where('id',$id)->update($request->all());
+      if ($post){
+
+          return $this->ShowResponse(null,"تمت العملية بنجاح",290);
+      }
+
+      return $this->ShowResponse(null,"فشلت العملية ",280);
 
 
   }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/post',[PostController::class,'index'])->name('index');
 Route::get('/post/{id}',[PostController::class,'show'])->name('show');
+
+Route::post('/post',[PostController::class,'store'])->name('store');
+
+Route::delete('/post/{id}',[PostController::class,'destroy'])->name('destroy');
+Route::put('/post/{id}',[PostController::class,'update'])->name('update');
